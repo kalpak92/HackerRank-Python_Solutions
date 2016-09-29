a6264bb8720a245d6975e7f06ecc77e2e15a91c8
Kalpak Seal <kals9seals@gmail.com>
2016-09-29 11:52:22 +0530

    Delete stringSplitAndJoin.py

--- 3.%20Strings/stringSplitAndJoin.py
+++
@@ -1,9 +0,0 @@
-'''
-Title     : String split and join
-Subdomain : Strings
-Domain    : Python
-Author    : Kalpak Seal
-Created   : 29 September 2016
-'''
-
-print '-'.join(raw_input().strip().split(' '))
